b49579e82cb9954f09c3e981cdf492c4f0925b0a
SKOLZ <gabriel.zanzotti@wolox.com.ar>
2016-07-31 04:49:31 -0300

    asd2

--- Assets/web_socket.cs
+++ Assets/web_socket.cs
@@ -6,15 +6,6 @@ using WebSocketSharp;
 using SimpleJSON;
 
 public class web_socket : MonoBehaviour {
-<<<<<<< HEAD
-	public Text text;
-	// Use this for initialization
-	void Start () {
-		Debug.Log("start");
-		//using (var ws = new WebSocket ("ws://10.0.0.22:8080")) {
-		var ws = new WebSocket ("ws://10.0.0.59:8080");
-			Debug.Log("ws");
-=======
 	public Text speed;
 	public Text distance;
 	public Text time;
@@ -28,18 +19,11 @@ public class web_socket : MonoBehaviour {
 	void Start () {
 		//using (var ws = new WebSocket ("ws://10.0.0.22:8080")) {
 		var ws = new WebSocket ("ws://10.0.0.165:8080");
->>>>>>> 042b805770b56180db3dd498ad56f05a3c130dac
 			ws.OnOpen += (sender, e) => {
 				Debug.Log("Open: ");
 			};
 			ws.OnMessage += (sender, e) => {
 				var message = JSON.Parse(e.Data);
-<<<<<<< HEAD
-				Debug.Log("Speed: " + message["speed"]);
-				Debug.Log("Speed: " + message["speed"]);
-				Debug.Log("Speed: " + message["speed"]);
-=======
-				value_speed = float.Parse(message["speed"]);
 				data_speed = Math.Round(value_speed)  + " KM/H";
 				data_distance = message["distance"] + " KM";
 				var raw_time = float.Parse(message["time"]);
@@ -47,7 +31,6 @@ public class web_socket : MonoBehaviour {
 				var minutes = Math.Round(raw_time % 60).ToString("00");
 				data_time = seconds + ":" + minutes;
 				data_resting = message["resting"] == "true";
->>>>>>> 042b805770b56180db3dd498ad56f05a3c130dac
 			};
 			ws.OnClose += (sender, e) => {
 				Debug.Log("Close:" + e.Code);
@@ -61,9 +44,6 @@ public class web_socket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-<<<<<<< HEAD
-		
-=======
 		if(speed.text != data_speed) {
 			speed.text = data_speed;
 		}
@@ -81,6 +61,5 @@ public class web_socket : MonoBehaviour {
 
 	public Boolean getResting() {
 		return data_resting;
->>>>>>> 042b805770b56180db3dd498ad56f05a3c130dac
 	}
 }
