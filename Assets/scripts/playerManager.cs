d6287b84978d2db898405a4662d2b4cade33ba56
Pablo Ferro <pablo.pyp@gmail.com>
2016-07-31 02:02:14 -0300

    Using ws speed

--- Assets/scripts/playerManager.cs
+++ Assets/scripts/playerManager.cs
@@ -2,26 +2,35 @@
 using System.Collections;
 
 public class playerManager : MonoBehaviour {
+	public web_socket ws;
 
 	public enum PlayerState {
 		RIDING = 0,
 		RESTING = 1
 	}
 
-	public float speed;
+	private float speed;
 
 	private PlayerState state;
 	
 	void Start () {
-		state = PlayerState.RIDING;
+		state = PlayerState.RESTING;
+		speed = 0;
 	}
 
 	void Update () {
-		if (isRiding()) {
-			// get speed
+		speed = ws.getSpeed ();
+		if (isRiding ()) {
 			Vector3 newPos = transform.position;
 			newPos.z += speed * Time.deltaTime;
 			transform.position = newPos;
+			if (speed == 0) {
+				state = PlayerState.RESTING;
+			}
+		} else {
+			if (speed != 0) {
+				state = PlayerState.RIDING;
+			}
 		}
 	}
 
--- Assets/web_socket.cs
+++ Assets/web_socket.cs
@@ -6,21 +6,28 @@ using WebSocketSharp;
 using SimpleJSON;
 
 public class web_socket : MonoBehaviour {
-	public Text text;
+	public Text speed;
+	public Text distance;
+	public Text time;
+
+	private string data_speed = "0";
+	private string data_distance = "0";
+	private string data_time = "0";
 	// Use this for initialization
 	void Start () {
-		Debug.Log("start");
 		//using (var ws = new WebSocket ("ws://10.0.0.22:8080")) {
-		var ws = new WebSocket ("ws://10.0.0.59:8080");
-			Debug.Log("ws");
+		var ws = new WebSocket ("ws://10.0.0.165:8080");
 			ws.OnOpen += (sender, e) => {
 				Debug.Log("Open: ");
 			};
 			ws.OnMessage += (sender, e) => {
 				var message = JSON.Parse(e.Data);
-				Debug.Log("Speed: " + message["speed"]);
-				Debug.Log("Speed: " + message["speed"]);
-				Debug.Log("Speed: " + message["speed"]);
+				data_speed = message["speed"];
+				data_distance = message["distance"];
+				data_time = message["time"];
+				// Debug.Log("Speed: " + message["speed"]);
+				// Debug.Log("Speed: " + message["speed"]);
+				// Debug.Log("Speed: " + message["speed"]);
 			};
 			ws.OnClose += (sender, e) => {
 				Debug.Log("Close:" + e.Code);
@@ -34,6 +41,18 @@ public class web_socket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(speed.text != data_speed) {
+			speed.text = data_speed + " KM/H";
+		}
+		if(distance.text != data_distance) {
+			distance.text = data_distance + " KM";
+		}
+		if(time.text != data_time) {
+			time.text = data_time;
+		}
+	}
+
+	public float getSpeed() {
+		return float.Parse (data_speed);
 	}
 }
