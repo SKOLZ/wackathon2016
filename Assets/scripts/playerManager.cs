3e93f61a72114552dd44af57974811b5f0ebcf64
SKOLZ <gabriel.zanzotti@wolox.com.ar>
2016-07-30 21:00:03 -0300

    added player and animation blabla

---
+++ Assets/scripts/playerManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerManager : MonoBehaviour {
+
+	public enum PlayerState {
+		RIDING = 0,
+		RESTING = 1
+	}
+
+	public float speed;
+
+	private PlayerState state;
+	
+	void Start () {
+		state = PlayerState.RIDING;
+	}
+
+	void Update () {
+		if (isRiding()) {
+			// get speed
+			Vector3 newPos = transform.position;
+			newPos.z += speed * Time.deltaTime;
+			transform.position = newPos;
+		}
+	}
+
+	public bool isRiding () {
+		return state == PlayerState.RIDING;
+	}
+
+	public bool isResting () {
+		return state == PlayerState.RESTING;
+	}
+}
