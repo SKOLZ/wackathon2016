011d22337ae3efd4f58c6df837b1d927362a04af
Pablo Ferro <pablo.pyp@gmail.com>
2016-07-31 00:02:04 -0300

    sockets

--- Assets/web_socket.cs
+++ Assets/web_socket.cs
@@ -1,28 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System;
 using WebSocketSharp;
 using SimpleJSON;
 
 public class web_socket : MonoBehaviour {
-
+	public Text text;
 	// Use this for initialization
 	void Start () {
 		Debug.Log("start");
 		//using (var ws = new WebSocket ("ws://10.0.0.22:8080")) {
-		var ws = new WebSocket ("ws://10.0.0.51:8080");
+		var ws = new WebSocket ("ws://10.0.0.59:8080");
 			Debug.Log("ws");
 			ws.OnOpen += (sender, e) => {
 				Debug.Log("Open: ");
 			};
 			ws.OnMessage += (sender, e) => {
 				var message = JSON.Parse(e.Data);
-				// Debug.Log("SensorTag says: " + e.Data);
-				// Debug.Log("Timestamp: " + message["timestamp"]);
-				Debug.Log("Acc: (" + message["x"] + ", "  + 
-									 message["y"] + ", "  +
-									 message["z"] + ")");
-				Debug.Log("Mod: " + message["mod"]);
+				Debug.Log("Speed: " + message["speed"]);
+				Debug.Log("Speed: " + message["speed"]);
 				Debug.Log("Speed: " + message["speed"]);
 			};
 			ws.OnClose += (sender, e) => {
